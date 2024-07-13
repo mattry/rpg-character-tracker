9793385df71e0f71c0e11bea79976517ce6fd935
Matthew Ryan <84721683+mathyouryan@users.noreply.github.com>
2024-07-13 13:21:43 -0400

    Initial push of Spring Initializr files

---
+++ src/main/java/com/example/rpg_character_tracker/RpgCharacterTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rpg_character_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgCharacterTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgCharacterTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rpg_character_tracker/RpgCharacterTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rpg_character_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgCharacterTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
