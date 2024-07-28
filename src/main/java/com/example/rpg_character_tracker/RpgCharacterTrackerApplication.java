1e019ddef9de706e8e519498b1fdf950d90731ba
Matthew Ryan <84721683+mathyouryan@users.noreply.github.com>
2024-07-28 12:12:53 -0400

    Implement CommandLineRunner to create test character in DB when backend is ran.

--- src/main/java/com/example/rpg_character_tracker/RpgCharacterTrackerApplication.java
+++ src/main/java/com/example/rpg_character_tracker/RpgCharacterTrackerApplication.java
@@ -1,13 +1,32 @@
 package com.example.rpg_character_tracker;
 
+import com.example.rpg_character_tracker.model.Character;
+import com.example.rpg_character_tracker.service.CharacterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RpgCharacterTrackerApplication {
+public class RpgCharacterTrackerApplication implements CommandLineRunner {
+
+	@Autowired
+	private CharacterService characterService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RpgCharacterTrackerApplication.class, args);
+	}
+
+	// make a default character in the database for testing
+	@Override
+	public void run(String... args) throws Exception {
+		Character testCharacter = new Character(
+			"Testermann",
+			"Tester",
+			10, 10, 10, 10, 10, 10
+		);
+
+		characterService.createCharacter(testCharacter);
 	}
 
 }
